6083e3af6d95b916ce0a5dc0ad1a501b4c2bf200
Konagi-iz <cmaxwsl@gmail.com>
2023-11-25 01:04:14 +0900

    save

--- js/1-2.js
+++ js/1-2.js
@@ -1,10 +1,19 @@
 // 変数を使ってみよう！！
-const kani = "tako";
+// const 変数名 = 値;
+
+// コンソールで確認する
+// console.log(変数名)
+
+// 変数の値を変えてみよう
+
+
+$(".h2").css("background", "#15e315");
+$(".txt").css("background", "#15e315");
+$(".list__item").css("background", "#15e315");
 
 // 変数の種類
-const ebi = "ebi";
+const kani = "tako";
 
 let sakana = "sakana";
 
 var wakame = "wakame";
-
